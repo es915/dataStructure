ee819c15d0c6d726fc870128e113a3ef19b203e6
eulseon <95990910+es915@users.noreply.github.com>
2022-01-01 14:34:59 +0900

    12 / 31 / Fri (2) lock paper scissors

--- main.c
+++ main.c
@@ -1,53 +1,44 @@
 #include <stdio.h>
-
-// 버블 정렬 -> 오름차순
-void BubbleSort(int arr[], int n) {
-	int i, j, temp;
-	
-	for(i=0; i<n-1; i++) {
-		for(j=0; j<n-1-i; j++) {
-			if(arr[j]>arr[j+1]) {
-				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
-			}	
-		}
-	}
-}
-
-void SelectionSort(int arr[], int n) {
-    int i, j, temp, minIdx;
-
-    for(i=0; i<n-1; i++) {
-        minIdx = i;
-        for(j=1+i; j<n; j++) {
-            if(arr[minIdx] > arr[j]) {
-                minIdx = j;
-            }   
-        }
-        
-        temp = arr[j];
-        arr[j] = arr[minIdx];
-        arr[minIdx] = temp;
-    }
-
-    // 1. 배열의 처음부터 끝까지 탐색 -> 최솟값 찾기
-    // 2. 찾은 최솟값은 첫번째 위치에 배정
-    // 3. 배정을 끝내고 2번째 위치 탐색
-    // 4. N-1번 반복
-}
-
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
-	int arr[5] = {5, 3, 2, 4, 1};
-	
-	// BubbleSort(arr, sizeof(arr)/sizeof(int));
-    SelectionSort(arr, sizeof(arr)/sizeof(int));
 
-	int i;
-	
-	for(i=0; i<5; i++) {
-		printf("%d ", arr[i]);
-	}	
-	return 0;
+    srand(time(NULL));
+    // 1970년도 부터 현재까지의 시간을 초단위로 셈
+    // -> 숫자가 달라짐 -> 그에 따른 새로운 난수들이 출력됨
+
+    // 1에서 6까지 난수 출력
+    printf("주사위 결과 : %d\n", rand()%6 + 1);
+    
+    int human, computer, due=0, win=0, lose=0;
+    while(1) {
+        printf("가위 1, 바위 2, 보 3\n");
+        printf("내려고 하는 값 : ");
+        scanf("%d", &human);
+
+        computer = rand()%3 + 1;
+
+        if(human==computer) {
+            printf("Due\n");
+            due++;
+        } else if(human==1&&computer==3) {
+            printf("Win\n");
+            win++;
+        } else if(human==2&&computer==1) {
+            printf("Win\n");
+            win++;
+        } else if(human==3&&computer==2) {
+            printf("Win\n");
+            win++;
+        } else {
+            printf("Lose\n");
+            lose++;
+            break;
+        }
+    }
+
+    printf("승 : %d, 무 : %d, 패 : %d\n", win, due, lose);
+
+    return 0;
 }
